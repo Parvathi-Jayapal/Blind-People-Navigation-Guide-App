b6a56a527c9697fba5fea1a947ebb1ed34297faf
Parvathi-Jayapal <77677826+Parvathi-Jayapal@users.noreply.github.com>
2021-07-13 21:45:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+var Content;
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier=ml5.imageClassifier('Mobilenet',modelloaded)
+}
+function draw(){
+  image(video,0,0,300,300);
+  classifier.classify(video,gotResult);
+}
+function modelloaded(){
+  console.log("Model Loaded!")
+}
+function gotResult(error,results){
+if (error){
+  console.error(error);
+} else{
+  setTimeout(function()
+    { 
+        Content=results[0].label;
+        speak(Content);
+        console.log("Content in Timeout"+Content);
+
+    }, 5000);
+  console.log(results);
+  document.getElementById("result_object_name").innerHTML=results[0].label;
+  document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+  
+}}
+function speak(Content){
+var synth=window.speechSynthesis;
+console.log("Content in Timeout"+Content);
+speak_data=Content;
+var utterThis= new SpeechSynthesisUtterance(speak_data);
+synth.speak(utterThis);
+}
